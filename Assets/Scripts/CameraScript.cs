9487994454cd6433a2209f330f131e1f89aeed21
Jake Hills <jhills0022@yahoo.com>
2019-08-17 02:13:09 -0400

    initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private int i = 0; 
+
+    [SerializeField] SpriteRenderer[] cliffside;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        foreach (SpriteRenderer layer in cliffside)
+        {
+
+            if (i <= cliffside.Length - 1)
+            {
+                float orthoSize = cliffside[i].bounds.size.x * Screen.height / Screen.width * 0.5f;
+
+                Camera.main.orthographicSize = orthoSize;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
