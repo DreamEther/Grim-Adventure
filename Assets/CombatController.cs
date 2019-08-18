d832222425413d86784c65f015997b4de03831e4
Jake Hills <jhills0022@yahoo.com>
2019-08-17 23:57:58 -0400

    added enemy and player movement...though MovePosition isn't working atm

---
+++ Assets/CombatController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatController : MonoBehaviour
+{
+    [SerializeField] GameObject lane1P;
+    [SerializeField] GameObject lane1E;
+    [SerializeField] GameObject ninjaPrefab;
+    [SerializeField] GameObject dragonPrefab;
+    PlayerController playerController;
+    GameObject ninja;
+    float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        lane1P = GameObject.FindGameObjectWithTag("Lane1P");
+        lane1E = GameObject.FindGameObjectWithTag("Lane1E");
+
+        SpawnEnemy();
+        SpawnPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayerInLaneOne(ninja);
+    }
+
+    private void SpawnPlayer()
+    {
+
+        ninja = Instantiate(ninjaPrefab, lane1P.transform.position, Quaternion.identity);
+        MovePlayerInLaneOne(ninja);
+    }
+
+    private void MovePlayerInLaneOne(GameObject playerInLaneOne)
+    {
+        playerInLaneOne.GetComponent<Rigidbody2D>().MovePosition(lane1E.transform.position * speed * Time.deltaTime);
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(dragonPrefab, lane1E.transform.position, Quaternion.LookRotation(dragonPrefab.transform.forward));
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector3 minPlayerPos;
+    public Vector3 maxPlayerPos;
+    [SerializeField] float speed = 2f;
+    public enum PlayerStates { InCombatMyTurn, InCombatTheirTurn }
+
+    PlayerStates myTurn = PlayerStates.InCombatMyTurn;
+
+    private Animator runAnim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        runAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        runAnim.SetBool("BeginLaneCharge", true);
+        PlayerMovement();
+        //runAnim.Play("NinjaSwordRunning");
+    }
+
+    public void PlayerMovement() // this function will be called on 'Attack' button click!
+    {
+      //  var deltaX = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+       // var newXPos = transform.position.x + deltaX;
+      //  transform.position = new Vector3(Mathf.Clamp(transform.position.x + deltaX, minPlayerPos.x, maxPlayerPos.x), 0, 0);
+      
+     
     }
 }
