42241682ec586f3df337bd6114ddd8db01ee9dbc
Jake Hills <jhills0022@yahoo.com>
2019-08-31 17:14:55 -0400

    added minion art icons

---
+++ Assets/Scripts/Base Abstract Classes/BaseMinion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseMinion : ScriptableObject, IEnemyMinion
+{
+    public Sprite aMinionType;
+
+    public abstract void Initialize(GameObject gameObject);
+
+    public abstract void Ability();
+    public abstract void CounterAnim();
+    public abstract void TakingDamageAnim();
+    public abstract void TakeDamage(); // going to implement ITakeDamage interface
+}
+
+
