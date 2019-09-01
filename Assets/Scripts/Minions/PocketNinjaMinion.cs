c5c634e107e2ade49424553f6f2f778624dad234
Jake Hills <jhills0022@yahoo.com>
2019-09-01 03:10:56 -0400

    resized chars and made some folders for scripts

---
+++ Assets/Scripts/Minions/PocketNinjaMinion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Minion - Pocket Ninja")]
+public class PocketNinjaMinion : BaseMinion
+{
+
+    public int health = 20;
+    public int attackPower = 3;
+    PocketNinjaBehavior pocketNinjaBehavior;
+
+    public override void Initialize(GameObject gameObject)
+    {
+        pocketNinjaBehavior = gameObject.GetComponent<PocketNinjaBehavior>();
+        pocketNinjaBehavior.health = health;
+        pocketNinjaBehavior.attackPower = attackPower;
+
+    }
+
+    public override void Ability()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void CounterAnim()
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void TakingDamageAnim()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void TakeDamage()
+    {
+        throw new System.NotImplementedException();
+    }
+}
